8683cbe198ce96fd3e8ad364aaa2c591ccc333d0
krissada-bunthong <krissada_bunthong@cmu.ac.th>
2018-11-13 20:55:57 +0700

    - Create static , templates - Add bulmaCss to Project

--- member/views.py
+++ member/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>TEST</h1>')
+    return render(request,'member/index.html')
