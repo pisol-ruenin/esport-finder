7db77abe0981f3705743d67a290fa307982b12e5
krissada-bunthong <krissada_bunthong@cmu.ac.th>
2018-11-15 00:07:15 +0700

    Create index profile home signup login page :Kong

--- member/urls.py
+++ member/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
+# from django.urls import path
+from django.conf.urls import url , include
 from . import views
 
+
 urlpatterns = [
-    path('',views.index)
+    url(r'^$',views.index,name='index'),
+    url(r'^login$',views.login,name='login'),
+    url(r'^signup$',views.signup,name='signup'),
+    url(r'^home$',views.home,name='home'),
+    url(r'^profile$',views.profile,name='profile'),
+    # path('home/',views.signup),
     # path('<code_name>/',views.profile)
 ]
--- member/views.py
+++ member/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+
+
 def index(request):
-    return render(request,'member/index.html')
+    return render(request, 'member/index.html')
+
+
+def login(request):
+    return render(request, 'member/login.html')
+
+def signup(request):
+    return render(request, 'member/signup.html')
+
+def home(request):
+    return render(request, 'member/home.html')
+
+def profile(request):
+    return render(request, 'member/profile.html')
